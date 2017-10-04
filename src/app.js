2d62fb9726aac96b79f697589a52fdce34f400ff
Ashok Yadav <yadaas@amazon.com>
2017-10-04 20:18:48 +0530

    Added firebase initialization

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp({
+		    apiKey: "AIzaSyDufv0ixlM371BwuY4yBPbra5uTFyaF144",
+		    authDomain: "authentication-5c900.firebaseapp.com",
+		    databaseURL: "https://authentication-5c900.firebaseio.com",
+		    projectId: "authentication-5c900",
+		    storageBucket: "authentication-5c900.appspot.com",
+		    messagingSenderId: "1062540670476"
+	  	});
+	}
+
 	render() {
 		return (
 			<View>
-				<Text>An App!</Text>
+				<Header headerText="Authentication" />
 			</View>
 		);
 	};
