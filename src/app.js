39a0b6a47e77cd415744cb022bc66bc869cde229
Ashok Yadav <yadaas@amazon.com>
2017-10-09 11:14:51 +0530

    Added Login Form

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { Header } from './components/common';
 import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
 	componentWillMount() {
@@ -19,6 +20,7 @@ class App extends Component {
 		return (
 			<View>
 				<Header headerText="Authentication" />
+				<LoginForm />
 			</View>
 		);
 	};
---
+++ src/components/LoginForm.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Button, Card, CardSection } from './common';
+
+class LoginForm extends Component {
+	render() {
+		return (
+			<Card>
+				<CardSection>
+				</CardSection>
+
+				<CardSection>
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Log in
+					</Button>
+				</CardSection>
+			</Card>
+		)
+	}
+}
+
+export default LoginForm;
